7e8330e09c5ef6fd74feb533daa9f9b8683df5ed
jayanth-madupalli <jayanth.madupalli@gmail.com>
2019-03-10 13:27:14 +0530

    Initial Commit

--- UrlShorten/src/shortener/management/commands/refreshcodes.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from shortener.models import Shorten
-class Command(BaseCommand):
-    help = 'Refreshes all shortcodes'
-
-
-    def handle(self, *args, **options):
-        return Shorten.objects.ref_shortcodes()
